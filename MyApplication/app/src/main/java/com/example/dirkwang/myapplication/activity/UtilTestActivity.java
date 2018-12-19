d70da17590b75431427dda6578c1c4979c3229f1
dirkwang <dirkwang@tencent.com>
2018-12-19 20:11:49 +0800

    [o]测试代码

---
+++ MyApplication/app/src/main/java/com/example/dirkwang/myapplication/activity/UtilTestActivity.java
@@ -0,0 +1,15 @@
+package com.example.dirkwang.myapplication.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.example.dirkwang.myapplication.R;
+
+public class UtilTestActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_util_test);
+    }
+}
